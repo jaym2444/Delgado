9bec3e1e60cd59bcd978d937de89790193f506fc
Jay M <jay.mann244@gmail.com>
2020-10-11 01:35:07 -0500

    Added new style macro

---
+++ Delgado.SDK/Macro/IStyledMacro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Delgado.SDK.Macro
+{
+    public interface IStyledMacro : IMacro
+    {
+        /// <summary>
+        /// The CSS style that will be used by the HTML element in the page crafter
+        /// </summary>
+        public string Style { get; }
+
+        /// <summary>
+        /// The raw HTML of the macro element
+        /// </summary>
+        public string Html { get; }
+    }
+}
